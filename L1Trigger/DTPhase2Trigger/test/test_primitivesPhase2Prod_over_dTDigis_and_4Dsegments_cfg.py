810d8f00c384f952e8a94b5aaa47c60a3b37d4bc
Jaime Leon <jleonhol@gmail.com>
2019-10-23 11:07:47 +0200

    Add python txt and set test file to ST analysis

--- L1Trigger/DTPhase2Trigger/test/test_primitivesPhase2Prod_over_dTDigis_and_4Dsegments_cfg.py
+++ L1Trigger/DTPhase2Trigger/test/test_primitivesPhase2Prod_over_dTDigis_and_4Dsegments_cfg.py
@@ -16,10 +16,37 @@ process.load("L1Trigger.DTPhase2Trigger.dtTriggerPhase2PrimitiveDigis_cfi")
 
 process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('file:/eos/cms/store/user/folguera/P2L1TUpgrade/digis_segments_Run2016BSingleMuonRAW-RECO_camilo.root'))
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
-process.dtTriggerPhase2PrimitiveDigis.dump = True
-process.dtTriggerPhase2PrimitiveDigis.debug = False 
+process.dtTriggerPhase2PrimitiveDigis.dump = False
+process.dtTriggerPhase2PrimitiveDigis.debug = False
+process.dtTriggerPhase2PrimitiveDigis.chi2Th = cms.untracked.double(0.16)
+process.dtTriggerPhase2PrimitiveDigis.use_LSB = True
+
+#python tecno30
+process.dtTriggerPhase2PrimitiveDigis.printPython = True
+process.dtTriggerPhase2PrimitiveDigis.printHits = True 
+
+####################### SliceTest specials ##############################
+
+#Chi2 -> Changing a lot lately
+process.dtTriggerPhase2PrimitiveDigis.chi2Th = cms.untracked.double(0.16)
+
+#LSB -> Position 0.025 cm instead of 0.004 cm
+process.dtTriggerPhase2PrimitiveDigis.use_LSB = True
+process.dtTriggerPhase2PrimitiveDigis.x_precision = cms.untracked.double(0.025)
+process.dtTriggerPhase2PrimitiveDigis.tanPsi_precision = cms.untracked.double(1./4096.)
+
+#Correlate with BX
+process.dtTriggerPhase2PrimitiveDigis.useBX_correlation = True
+process.dtTriggerPhase2PrimitiveDigis.dBX_correlate_TP = 1
+
+#Correlate with tanPsi
+process.dtTriggerPhase2PrimitiveDigis.dTanPsi_correlate_TP = cms.untracked.double(900./4096.)
 process.dtTriggerPhase2PrimitiveDigis.grouping_code = cms.untracked.int32(0)
-process.dtTriggerPhase2PrimitiveDigis.clean_chi2_correlation = False
+
+#Confirmation forbidden
+process.dtTriggerPhase2PrimitiveDigis.allow_confirmation = False
+
+
 
 process.out = cms.OutputModule("PoolOutputModule",
                                outputCommands = cms.untracked.vstring('keep *'),
