bdf953690f9006c20926af418cbcd95d63569914
Jaime Leon <jleonhol@gmail.com>
2019-11-15 14:54:28 +0100

    Changing test file

--- L1Trigger/DTPhase2Trigger/test/test_primitivesPhase2Prod_over_dTDigis_and_4Dsegments_cfg.py
+++ L1Trigger/DTPhase2Trigger/test/test_primitivesPhase2Prod_over_dTDigis_and_4Dsegments_cfg.py
@@ -14,11 +14,11 @@ process.GlobalTag.globaltag = "80X_dataRun2_2016SeptRepro_v7"
 process.load("Phase2L1Trigger.CalibratedDigis.CalibratedDigis_cfi")
 process.load("L1Trigger.DTPhase2Trigger.dtTriggerPhase2PrimitiveDigis_cfi")
 
-process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('file:/eos/cms/store/user/folguera/P2L1TUpgrade/digis_segments_Run2016BSingleMuonRAW-RECO_camilo.root'))
-#process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('file:/eos/cms/store/user/folguera/P2L1TUpgrade/digis_segments_Run2016BSingleMuonRAW-RECO_camilo.root'),skipEvents=cms.untracked.uint32(988))
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
+#process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('file:/eos/cms/store/user/folguera/P2L1TUpgrade/digis_segments_Run2016BSingleMuonRAW-RECO_camilo.root'))
+process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring('file:/eos/cms/store/user/folguera/P2L1TUpgrade/digis_segments_Run2016BSingleMuonRAW-RECO_camilo.root'),skipEvents=cms.untracked.uint32(1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 process.dtTriggerPhase2PrimitiveDigis.dump = False
-process.dtTriggerPhase2PrimitiveDigis.debug = False
+process.dtTriggerPhase2PrimitiveDigis.debug = True
 process.dtTriggerPhase2PrimitiveDigis.chi2Th = cms.untracked.double(0.16)
 
 #scenario
@@ -36,8 +36,9 @@ process.dtTriggerPhase2PrimitiveDigis.printHits = False
 process.dtTriggerPhase2PrimitiveDigis.chi2Th = cms.untracked.double(0.16)
 
 #LSB -> Position 0.025 cm instead of 0.004 cm
-process.dtTriggerPhase2PrimitiveDigis.use_LSB = False
-process.dtTriggerPhase2PrimitiveDigis.x_precision = cms.untracked.double(0.025)
+process.dtTriggerPhase2PrimitiveDigis.use_LSB = True
+process.dtTriggerPhase2PrimitiveDigis.x_precision = cms.untracked.double(1./(10.*16.))
+#process.dtTriggerPhase2PrimitiveDigis.x_precision = cms.untracked.double(0.025)
 process.dtTriggerPhase2PrimitiveDigis.tanPsi_precision = cms.untracked.double(1./4096.)
 
 #Correlate with BX
@@ -45,11 +46,14 @@ process.dtTriggerPhase2PrimitiveDigis.useBX_correlation = True
 process.dtTriggerPhase2PrimitiveDigis.dBX_correlate_TP = 1
 
 #Correlate with tanPsi
-process.dtTriggerPhase2PrimitiveDigis.dTanPsi_correlate_TP = cms.untracked.double(900./4096.)
+process.dtTriggerPhase2PrimitiveDigis.dTanPsi_correlate_TP = cms.untracked.double(9999./4096.)
+#process.dtTriggerPhase2PrimitiveDigis.dTanPsi_correlate_TP = cms.untracked.double(900./4096.)
 
 #Confirmation forbidden
 process.dtTriggerPhase2PrimitiveDigis.allow_confirmation = False
 
+#TanPsi stuff
+process.dtTriggerPhase2PrimitiveDigis.tanPhiTh = cms.untracked.double(1.)
 
 
 process.out = cms.OutputModule("PoolOutputModule",
