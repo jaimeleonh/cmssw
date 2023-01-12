8cf32f3f950e50ffb957cc6d26f0fe57d476e879
Jaime Leon <jleonhol@gmail.com>
2023-01-12 10:59:40 +0100

    Adapted scripts to run from PT/NanoAOD/test

--- PhysicsTools/NanoAOD/test/executable_template.sh
+++ PhysicsTools/NanoAOD/test/executable_template.sh
@@ -2,7 +2,7 @@
 export X509_USER_PROXY="{PROXYPATH}"
 cd {CMSSWSETUP}
 eval `scramv1 runtime -sh`
-cd {JOBFOLDER}
+cd PhysicsTools/NanoAOD/test/{JOBFOLDER}
 cmsRun {CFGNAME}
 echo 'sending the file back'
 
--- PhysicsTools/NanoAOD/test/job_creator.py
+++ PhysicsTools/NanoAOD/test/job_creator.py
@@ -4,13 +4,18 @@ cfg_name = "NANO_NANO_template.py"
 executable_name = "executable_template.sh"
 df_name = "df_template.py"
 user_proxy = "/afs/cern.ch/work/j/jleonhol/private/L1/nanoaod/CMSSW_12_6_0_patch1/src/x509up_u124088"
-cmssw_setup = "/".join(os.path.abspath(__file__).split("/")[0:-1])
+cmssw_setup = os.path.join(os.environ["CMSSW_BASE"], "src/")
 
-if len(sys.argv) != 3:
-    print("Use: python3 NANO_NANO_template.py inputfiles outputfolder")
 
-input_files_filename = sys.argv[1]
-output_folder = sys.argv[2]
+import argparse
+parser = argparse.ArgumentParser(description='options')
+parser.add_argument('-i','--input', dest='input', required=True)
+parser.add_argument('-o','--output', dest='output', required=True)
+parser.add_argument('-n','--nfiles', type=int, dest='nfiles', default=1)
+options = parser.parse_args()
+
+input_files_filename = options.input
+output_folder = options.output
 
 with open(input_files_filename) as f:
     input_files = [filename.strip() for filename in f.readlines()]
@@ -31,7 +36,14 @@ if os.path.isdir(job_folder):
 
 os.mkdir(job_folder)
 
-for ifile, f in enumerate(input_files):
+# for ifile, f in enumerate(input_files):
+for ifile in range(0, len(input_files), options.nfiles):
+    if options.nfiles == 1:
+        f = '"%s"' % input_files[ifile]
+    else:
+        max_ifile = min(ifile + options.nfiles, len(input_files))
+        f = ", ".join(['"%s"' % elem for elem in input_files[ifile:max_ifile]])
+        ifile = ifile // options.nfiles
     this_nano = cfg.replace("{INPUTFILENAME}", f).replace("{OUTPUTFILENAME}", "ntuple_%s.root" % ifile)
     os.mkdir("%s/Job_%s/" % (job_folder, ifile))
     with open("%s/Job_%s/cfg.py" % (job_folder, ifile), "w+") as f2:
