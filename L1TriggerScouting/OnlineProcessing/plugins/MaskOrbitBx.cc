46e2b2aa1c377b7ce1c4f0a54f76eb9623c72fc4
Giovanni <gpetruc@gmail.com>
2025-09-25 10:53:20 +0200

    Rebase of L1 Scouting Phase-2 code in https://github.com/gpetruc/cmssw/tree/15_1_X_scoutingPhase2 Contributions from: - Aravind Sugunan <aravindsugunan@gmail.com>   https://github.com/ats2008   (TkMuons, X --> mu mu) - Cecile Caillol <ccaillol@cern.ch>   https://github.com/cecilecaillol   (TkEm and TkEle, W->Ds gamma and W->pi gamma) - Giovanni Petrucciani <Giovanni.Petrucciani@cern.ch>   https://github.com/gpetruc   (framework, integration, W->3pi) - Kyungmin Park <kyungmin.park@cern.ch>   https://github.com/kyungminparkdrums   (dark photon -> ee) - Rocco Ardino <rocco.ardino@cern.ch>   https://github.com/RoccoA97   (Higgs rare decays)

--- L1TriggerScouting/OnlineProcessing/plugins/MaskOrbitBx.cc
+++ L1TriggerScouting/OnlineProcessing/plugins/MaskOrbitBx.cc
@@ -1,96 +1,9 @@
-#include <memory>
-#include <string>
-#include <utility>
-#include <vector>
+#include "L1TriggerScouting/OnlineProcessing/interface/MaskOrbitBx.h"
 
 #include "DataFormats/L1Scouting/interface/L1ScoutingMuon.h"
 #include "DataFormats/L1Scouting/interface/L1ScoutingCalo.h"
 #include "DataFormats/L1Scouting/interface/L1ScoutingBMTFStub.h"
-#include "DataFormats/L1Scouting/interface/L1ScoutingCaloTower.h"
-#include "DataFormats/L1Scouting/interface/L1ScoutingCaloJet.h"
-#include "DataFormats/L1Scouting/interface/OrbitCollection.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/stream/EDProducer.h"
-#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
-#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
-
-template <typename T>
-class MaskOrbitBx : public edm::stream::EDProducer<> {
-public:
-  explicit MaskOrbitBx(const edm::ParameterSet&);
-  ~MaskOrbitBx() override = default;
-
-  static void fillDescriptions(edm::ConfigurationDescriptions&);
-
-private:
-  void produce(edm::Event&, const edm::EventSetup&) override;
-
-  static constexpr int kNBX = 3565;
-
-  // token for scouting data
-  edm::EDGetTokenT<OrbitCollection<T>> const tokenData_;
-
-  // BXs to be kept
-  edm::EDGetTokenT<std::vector<unsigned>> const tokenSelBxs_;
-
-  std::string const productLabel_;
-
-  std::vector<std::vector<T>> orbitBuffer_;
-};
-
-template <typename T>
-MaskOrbitBx<T>::MaskOrbitBx(const edm::ParameterSet& iPSet)
-    : tokenData_(consumes(iPSet.getParameter<edm::InputTag>("dataTag"))),
-      tokenSelBxs_(consumes(iPSet.getParameter<edm::InputTag>("selectBxs"))),
-      productLabel_(iPSet.getParameter<std::string>("productLabel")) {
-  // prepare module buffer
-  orbitBuffer_ = std::vector<std::vector<T>>(kNBX);
-
-  // products
-  produces<OrbitCollection<T>>(productLabel_).setBranchAlias(productLabel_ + "OrbitCollection");
-}
-
-// ------------ method called for each ORBIT  ------------
-template <typename T>
-void MaskOrbitBx<T>::produce(edm::Event& iEvent, const edm::EventSetup&) {
-  // get selected BXs
-  edm::Handle<std::vector<unsigned>> selBxs;
-  iEvent.getByToken(tokenSelBxs_, selBxs);
-
-  // get the data
-  edm::Handle<OrbitCollection<T>> objCollection;
-  iEvent.getByToken(tokenData_, objCollection);
-
-  // prepare new collections
-  std::unique_ptr<OrbitCollection<T>> selectedObjs(new OrbitCollection<T>);
-
-  int nObjOrbit = 0;
-
-  // fill collections with objects
-  for (auto const bx : *selBxs) {
-    auto const& objs = objCollection->bxIterator(bx);
-    orbitBuffer_[bx].reserve(objs.size());
-    for (auto const& obj : objs) {
-      orbitBuffer_[bx].push_back(obj);
-      nObjOrbit++;
-    }
-  }
-
-  // fill orbit collection and clear the Bx buffer vector
-  selectedObjs->fillAndClear(orbitBuffer_, nObjOrbit);
-
-  // store collections in the event
-  iEvent.put(std::move(selectedObjs), productLabel_);
-}
-
-template <typename T>
-void MaskOrbitBx<T>::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  edm::ParameterSetDescription desc;
-  desc.add<edm::InputTag>("dataTag");
-  desc.add<edm::InputTag>("selectBxs");
-  desc.add<std::string>("productLabel");
-  descriptions.addDefault(desc);
-}
+#include "FWCore/Framework/interface/MakerMacros.h"
 
 typedef MaskOrbitBx<l1ScoutingRun3::Muon> MaskOrbitBxScoutingMuon;
 typedef MaskOrbitBx<l1ScoutingRun3::Jet> MaskOrbitBxScoutingJet;
